60ff3b907ba2d5b0007c9951ebff5f59cf106f86
Hengguang <1370786129@qq.com>
2021-07-11 17:07:04 +0800

    test

---
+++ src/main/java/com/example/BitCoinAnalyzer/BitCoinAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.BitCoinAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitCoinAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitCoinAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BitCoinAnalyzer/BitCoinAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BitCoinAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitCoinAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
